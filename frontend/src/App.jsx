e383a68aa2e097132669e7a3d4a6b64da99956d5
eliav29 <liavma3@gmail.com>
2024-01-16 16:23:26 +0200

    after footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import Title from './Components/Shered/Title.jsx'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import {HomePage} from './Pages/HomePage.jsx'
+import Footer from './Components/Shered/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <div className='d-flex flex-column side-allPage min-width'>
+        {/* <Header/> */}
+        <main>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomePage/>} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer/>
+      </div>
+      <Title title="eliav">
+      </Title>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Title from '../Components/Shered/Title'
+
+export const HomePage = () => {
+  return (
+    
+    <div>
+        <Title title='home page'></Title>
+    HomePage
+    </div>
+  )
+}
+
+
+
+// rafce
