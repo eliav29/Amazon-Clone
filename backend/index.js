9f5db2929b32d93219ccaca31cd62d56d022b837
eliav29 <liavma3@gmail.com>
2024-01-16 15:33:29 +0200

    Connected to mongo with connection string

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import User from "./models/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -7,8 +12,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+
+const PORT = process.env.PORT || 8080;
+
 //routes:
 
-app.listen(8080, function() {
-    console.log("Server is running on http://localhost:8080");
+app.post('/addUser', async(req, res) => {
+    // const user = req.body.user
+    // const {user} = req.body;
+    const newUser = await User.create(req.body);
+    res.send(newUser);
+    // await User.create(user);
 });
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+.then(() => {
+    app.listen(PORT, function() {
+        console.log("Server is running on http://localhost:8080");
+    });
+}).catch(err => {console.log(err.message)});
